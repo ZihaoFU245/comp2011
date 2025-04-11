2918eaf16df00ef7da481597b0ec418ebf2efa1c
Zihao <zihaofu12@gmail.com>
2025-04-11 11:33:25 +0800

    nothing to commit, working tree clean

--- test.cpp
+++ test.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <memory>
 using namespace std;
+
+
+struct Point
+{
+    /* data */
+    float x, y;
+    Point(float x, float y) : x(x), y(y) {}
+};
+
 
 int main()
 {
-    int size{};
-    cin >> size;
-    cout << "Entered " << size << endl;
+    Point p1(1.0, 2.0);
+    cout << p1.x << ' ' << p1.y << endl;
+    Point *ptr = &p1;
+    ptr -> x = 3.0;
+    ptr -> y = 4.0;
+    cout << p1.x << ' ' << p1.y << endl;
 }
+
